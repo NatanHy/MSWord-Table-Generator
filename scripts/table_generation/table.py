ee045e6d953887ed3f0d88eb140b4d056bb1a813
Natan Hyötyläinen <hyotylainennatan@gmail.com>
2025-07-17 13:32:23 +0300

    Added back table.py (it was not unused)

---
+++ scripts/table_generation/table.py
@@ -0,0 +1,24 @@
+from docx.document import Document
+from dataclasses import dataclass
+import os
+
+@dataclass
+class Table:
+    doc : Document
+    source_path : str
+    geosphere_id : str
+
+    def save(self, output_dir, make_subfolder=True):
+        if make_subfolder:
+            subfolder_name = os.path.basename(self.source_path).replace(" ", "_")
+            full_subfolder_path = os.path.join(output_dir, subfolder_name)
+        else:
+            full_subfolder_path = output_dir
+
+        if not os.path.isdir(full_subfolder_path):
+            os.makedirs(full_subfolder_path)
+
+        save_path = os.path.join(full_subfolder_path, f"table_{self.geosphere_id}.docx")
+        save_path = os.path.normpath(save_path)
+        print(f"Saved table in {save_path}")
+        self.doc.save(save_path)
